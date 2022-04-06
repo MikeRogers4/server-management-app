aa0dcddb722d4ad9f1683db24871a15e7ec9f620
Mike Rogers <mrogers@pcs-publishing.com>
2022-04-06 11:48:56 +0100

    WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import * as osu from 'node-os-utils'
 import * as _ from 'lodash'
-import { ResponsivePie } from 'nivo'
+import { ResponsivePie } from '@nivo/pie'
 
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>
@@ -8,26 +8,48 @@ function Header({ title }) {
 
 export default function HomePage({ data }) {
   const pieData = [{
-    "id": "free",
-    "label": "free",
-    "value": parseInt(data.freeGb, 10),
-    "color": "hsl(126, 70%, 50%)"
+    id: 'free',
+    label: 'free',
+    value: parseInt(data.freeGb, 10),
+    color: '#A3A1FB'
   }, {
-    "id": "used",
-    "label": "used",
-    "value": parseInt(data.usedGb, 10),
-    "color": "hsl(219, 70%, 50%)"
+    id: 'used',
+    label: 'used',
+    value: parseInt(data.usedGb, 10),
+    color: '#58B5AB'
   }]
 
   return (
     <>
       <Header title={'Server Details'} />
       <p>{JSON.stringify(data)}</p>
-      <div style={{ height: 200 }}>
+      <div style={{ height: 250 }}>
         <ResponsivePie
           data={pieData}
-          width={200}
-          height={200}
+          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+          legends={[{
+            anchor: 'top',
+            direction: 'row',
+            justify: false,
+            translateX: 0,
+            translateY: 56,
+            itemsSpacing: 0,
+            itemWidth: 100,
+            itemHeight: 18,
+            itemTextColor: '#999',
+            itemDirection: 'left-to-right',
+            itemOpacity: 1,
+            symbolSize: 18,
+            symbolShape: 'circle',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemTextColor: '#000'
+                }
+              }
+            ]
+          }]}
         />
       </div>
     </>
