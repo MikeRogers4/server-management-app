d4ff74754be0aa64dd25968bf8c227e48df1175b
Mike Rogers <mrogers@pcs-publishing.com>
2022-04-12 12:47:28 +0100

    grid

--- components/grid.tsx
+++ components/grid.tsx
@@ -5,7 +5,9 @@ export default function Grid({ data, title, onClick }) {
     return <>
         <h1 className={styles.title}>{title}</h1>
         {data.map((datum, index) => (
-            <div key={`grid-${index}`} className={styles.item} onClick={onClick} data-running={datum.running}>{datum.name} {datum.running ? <FaCheck /> : <FaTimes />}</div>
+            <div key={`grid-${index}`} className={styles.item} onClick={onClick} data-running={datum.running}>
+                {datum.name} {datum.running ? <FaCheck /> : <FaTimes />}
+            </div>
         ))}
     </>
 }
